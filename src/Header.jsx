ccac88b27a7427d4282cd9518514d996e79e3872
Karthi P <karthi.miet@gmail.com>
2024-02-06 11:44:17 +0530

    vite config file updated

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <>
-        <div>Header</div>
+        <div>New Header</div>
     <h1>Header Component</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto laborum eligendi amet vel iste quam reiciendis, placeat ratione sapiente voluptas dolore veritatis consequuntur, exercitationem voluptate doloribus impedit hic neque error?</p>
     </>
